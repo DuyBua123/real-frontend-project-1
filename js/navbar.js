950d3dcbfa06b680562cd1d0ff43c02dfc246469
DuyBua <mondorea42@gmail.com>
2024-05-23 11:06:42 +0700

    Design hero image slide

--- js/navbar.js
+++ js/navbar.js
@@ -17,4 +17,34 @@ $(document).ready(() => {
             document.getElementById("navbar").style.top = "40px"
         }
     }
+
+    // Hero image slides
+    $("#hero-image-slide").slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        prevArrow: $("#prevArrow"),
+        nextArrow: $("#nextArrow"),
+        dots: true,
+        customPaging: () => {
+            return "<button></button>"
+        }
+    });
+
+
+    $("#outer-hero-image").on("mouseenter",() => {
+        const prevArrow = document.getElementById("prevArrow");
+        const nextArrow = document.getElementById("nextArrow");
+
+        prevArrow.style.marginLeft = "10px"
+        nextArrow.style.marginRight = "10px"
+
+    })
+    $("#outer-hero-image").on("mouseleave",() => {
+        const prevArrow = document.getElementById("prevArrow");
+        const nextArrow = document.getElementById("nextArrow");
+
+        prevArrow.style.marginLeft = "-35px"
+        nextArrow.style.marginRight = "-35px"
+
+    })
 });
