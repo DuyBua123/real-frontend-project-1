5838b7c79c864878a587423d4aba00c813ad7a72
DuyBua <mondorea42@gmail.com>
2024-05-23 07:03:31 +0700

    Design navbar

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+    // Change style of dropdown menu
+    $("#dropdown-btn").on("click", () => {
+        const menu = document.getElementById("dropdown-menu")
+        menu.style.height = "430px"
+    })
+    $("#dropdown-btn").on("mouseenter", () => {
+        const menu = document.getElementById("dropdown-menu")
+        menu.style.height = "0"
+    })
+
+    // Animate navbar when scrolling down
+    window.onscroll = () => {
+        if(document.documentElement.scrollTop > 1) {
+            document.getElementById("navbar").style.top = "0"
+        } else {
+            document.getElementById("navbar").style.top = "40px"
+        }
+    }
+});
